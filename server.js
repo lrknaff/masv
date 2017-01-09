8acb4df26ed92833d324a406601d663f75d48d94
lrknaff <lacey.knaff@gmail.com>
2017-01-09 15:25:19 -0700

    add webpack dev middleware for hot reloading

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var httpProxy = require('http-proxy');
 
 var proxy = httpProxy.createProxyServer();
 var app = express();
+var webpackMiddleware = require("webpack-dev-middleware");
 
 var isProduction = process.env.NODE_ENV === 'production';
 var port = isProduction ? process.env.PORT : 3000;
@@ -12,7 +13,6 @@ var port = isProduction ? process.env.PORT : 3000;
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use('/projects/:name', express.static(path.join(__dirname, 'projects')));
-
 // And run the server
 app.listen(port, function () {
   console.log('Server running on port ' + port);
